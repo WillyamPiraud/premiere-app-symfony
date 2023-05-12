f202303980fa464bb241c988fbd7b69474fb8e9f
Willyam Piraud <wawa69360@gmail.com>
2023-05-12 10:06:30 +0200

    Application terminé

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -20,6 +25,39 @@ class LoginController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $user->setRoles(['ROLE_USER']);
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user->setPassword(
+                $userPasswordHasherInterface->hashPassword($user, $user->getPassword())
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Vous êtes bien inscrit sur LocaJeu !'
+            );
+
+            return $this->redirectToRoute('app_login');
+
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
 
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout()
